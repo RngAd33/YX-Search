e88a931f5ce0066064829e12dd4ef0ab7040410b
RngAd33 <yxbsyx@outlook.com>
2025-10-01 15:52:08 +0800

    搜索功能开发

--- src/main/java/com/rngad33/yxsearch/datasource/PostDataSource.java
+++ src/main/java/com/rngad33/yxsearch/datasource/PostDataSource.java
@@ -1,13 +1,19 @@
 package com.rngad33.yxsearch.datasource;
 
+import cn.hutool.core.util.ObjUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.rngad33.yxsearch.common.ErrorCode;
+import com.rngad33.yxsearch.exception.ThrowUtils;
 import com.rngad33.yxsearch.model.dto.post.PostQueryRequest;
 import com.rngad33.yxsearch.model.entity.Post;
 import com.rngad33.yxsearch.model.vo.PostVO;
 import com.rngad33.yxsearch.service.PostService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 帖子数据源
@@ -21,9 +27,14 @@ public class PostDataSource implements DataSource<PostVO> {
     @Override
     public Page<PostVO> doSearch(String searchText, long pageNum, long pageSize) {
         PostQueryRequest postQueryRequest = new PostQueryRequest();
-        Page<Post> postPage = postService.page(new Page<>(pageNum, pageSize),
-                postService.getQueryWrapper(postQueryRequest));
-        return postService.getPostVOPage(postPage, null);
+        postQueryRequest.setSearchText(searchText);
+        postQueryRequest.setCurrent(pageNum);
+        postQueryRequest.setPageSize(pageSize);
+        // 取request
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        ThrowUtils.throwIf(ObjUtil.isNull(servletRequestAttributes), ErrorCode.OPERATION_ERROR);
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        return postService.listPostVOByPage(postQueryRequest, request);
     }
 
 }
